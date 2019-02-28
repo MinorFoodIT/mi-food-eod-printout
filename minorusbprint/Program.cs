8675a59a3dd525ab6c511b36250d986e5d9d1644
foodit <minorfoodit@gmail.com>
2019-02-28 09:46:12 +0700

    first commit

---
+++ minorusbprint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace minorusbprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string printerName = args[0];
+            string filename = args[1];
+            Console.WriteLine(printerName);
+            Console.WriteLine(filename);
+            //FileStream fs = new FileStream(filename, FileMode.Open);
+            Console.WriteLine(RawPrinterHelper.SendFileToPrinter(printerName, filename));
+            
+        }
+    }
+}
